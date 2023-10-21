f965c5b3dcd151797bc4877d315bd84f1fc03fbf
Minh Tran <ttminh@live.com>
2023-10-21 08:30:50 +0700

    update with db-migrate

---
+++ migrations/20231021011421-add-products.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = function (db, callback) {
+  db.createTable('products', {
+    id: { type: 'int', primaryKey: true, autoIncrement: true },
+    name: 'string',
+    price: 'int',
+    amount: 'int',
+    department_id : {
+      type: 'int',
+      foreignKey: {
+        name: 'products_department_id_fk',
+        table: 'departments',
+        rules: {
+          onDelete: 'CASCADE',
+          onUpdate: 'RESTRICT'
+        },
+        mapping: 'id'
+      }
+    },
+  }, callback);
+};
+
+exports.down = function (db, callback) {
+  db.dropTable('departments', callback);
+};
+
+exports._meta = {
+  "version": 1
+};
